58d3ac91d9da73b996fe31a1570b4fb9d62fd106
Stefan Ludwig <stefan.ludwig@slu-it.de>
2017-09-02 13:02:15 +0200

    more tests

--- src/test/kotlin/de/sluit/dnd/CharacterTest.kt
+++ src/test/kotlin/de/sluit/dnd/CharacterTest.kt
@@ -5,10 +5,7 @@ import de.sluit.dnd.proficiencies.AnimalHandling
 import de.sluit.dnd.proficiencies.Athletics
 import de.sluit.dnd.proficiencies.Intimidation
 import de.sluit.dnd.proficiencies.Survival
-import de.sluit.dnd.rolls.withAdvantage
-import org.assertj.core.api.Assertions
-import org.assertj.core.api.Assertions.*
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
@@ -71,6 +68,156 @@ internal class CharacterTest {
             assertThat(savingThrow.roll).isBetween(1, 20)
             assertThat(savingThrow.abilityModifier).isEqualTo(1)
             assertThat(savingThrow.proficiencyBonus).isEqualTo(0)
+        }
+
+    }
+
+    @Nested inner class `skill checks are calculated correctly` {
+
+        @Nested inner class `strength based` {
+
+            @Test fun `athletics`() {
+                val check = grog.athleticsCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(7)
+                assertThat(check.proficiencyBonus).isEqualTo(5)
+            }
+
+        }
+
+        @Nested inner class `dexterity based` {
+
+            @Test fun `acrobatics`() {
+                val check = grog.acrobaticsCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(2)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+            @Test fun `sleight of hand`() {
+                val check = grog.sleightOfHandCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(2)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+            @Test fun `stealth`() {
+                val check = grog.stealthCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(2)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+        }
+
+        @Nested inner class `intelligence based` {
+
+            @Test fun `arcana`() {
+                val check = grog.arcanaCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(-2)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+            @Test fun `history`() {
+                val check = grog.historyCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(-2)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+            @Test fun `investigation`() {
+                val check = grog.investigationCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(-2)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+            @Test fun `nature`() {
+                val check = grog.natureCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(-2)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+            @Test fun `religion`() {
+                val check = grog.religionCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(-2)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+        }
+
+        @Nested inner class `wisdom based` {
+
+            @Test fun `animal handling`() {
+                val check = grog.animalHandlingCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(0)
+                assertThat(check.proficiencyBonus).isEqualTo(5)
+            }
+
+            @Test fun `insight`() {
+                val check = grog.insightCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(0)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+            @Test fun `medicine`() {
+                val check = grog.medicineCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(0)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+            @Test fun `perception`() {
+                val check = grog.perceptionCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(0)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+            @Test fun `survival`() {
+                val check = grog.survivalCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(0)
+                assertThat(check.proficiencyBonus).isEqualTo(5)
+            }
+
+        }
+
+        @Nested inner class `charm based` {
+
+            @Test fun `deception`() {
+                val check = grog.deceptionCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(1)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+            @Test fun `intimidation`() {
+                val check = grog.intimidationCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(1)
+                assertThat(check.proficiencyBonus).isEqualTo(5)
+            }
+
+            @Test fun `performance`() {
+                val check = grog.performanceCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(1)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
+            @Test fun `persuasion`() {
+                val check = grog.persuasionCheck()
+                assertThat(check.roll).isBetween(1, 20)
+                assertThat(check.abilityModifier).isEqualTo(1)
+                assertThat(check.proficiencyBonus).isEqualTo(0)
+            }
+
         }
 
     }
